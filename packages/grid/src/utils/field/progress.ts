0892def14aa1fe7705445e06dce91d7c30a4556b
minlovehua <321512939@qq.com>
2024-12-24 18:18:27 +0800

    fix(grid): search for progress field

--- packages/grid/src/utils/field/progress.ts
+++ packages/grid/src/utils/field/progress.ts
@@ -1,9 +1,13 @@
+import { isEmpty } from 'lodash';
 import { AITableField } from '../../index';
 import { Field } from './field';
 
 export class ProgressField extends Field {
     cellFullText(transformValue: number, field: AITableField): string[] {
-        const fullText = `${transformValue}%`;
-        return [fullText];
+        let fullText: string[] = [];
+        if (!isEmpty(transformValue)) {
+            fullText.push(`${transformValue}%`);
+        }
+        return fullText;
     }
 }
