9f3c5e85ac7027f4aee595813161a601ad920fa8
pubuzhixing8 <pubuzhixing@gmail.com>
2025-01-06 11:47:39 +0800

    chore: improve class name

--- packages/grid/src/constants/table.ts
+++ packages/grid/src/constants/table.ts
@@ -23,7 +23,7 @@ export const AI_TABLE_FIELD_ADD_BUTTON_WIDTH = 100; // 添加列宽度
 export const AI_TABLE_FIELD_HEAD_ICON_GAP_SIZE = 8; // 字段表列头图标的间距
 export const AI_TABLE_FIELD_HEAD_MORE = 'AI_TABLE_FIELD_HEAD_MORE'; // 更多图标名称
 
-export const AI_TABLE_PREVENT_CLEAR_SELECTION_CLASS = '.ai-table-prohibit-clear-selection';
+export const AI_TABLE_PREVENT_CLEAR_SELECTION_CLASS = '.ai-table-prevent-clear-selection';
 
 export const AI_TABLE_ICON_COMMON_SIZE = 16; // 表格图标的通用尺寸
 export const AI_TABLE_CELL = 'AI_TABLE_CELL'; // 单元格标识
